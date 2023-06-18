b6302c49185b79c1b964d2b16b31618d425f9cd8
Ojifu <ahmedhijazi49@gmail.com>
2023-06-18 14:04:43 -0600

    added loop to calculate player gold from items for all players

--- src/game-data/game-data.controller.ts
+++ src/game-data/game-data.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { GameDataService } from './game-data.service';
 import { AllPlayers } from '@insightedge/insightedge-common';
+import { PlayerGold } from '@insightedge/insightedge-common/lib/shared/models/player-gold';
 
 @Controller('game')
 export class GameDataController {
@@ -9,5 +10,27 @@ export class GameDataController {
   @Get('latest')
   public async getLatest(): Promise<AllPlayers> {
     return this.gameDataService.getGameData();
+  }
+  @Get('item-gold/all')
+  public async getAllItemGold(): Promise<PlayerGold[]> {
+    const allPlayerGold: PlayerGold[] = [];
+
+    const allPlayerData = await this.gameDataService.getGameData();
+    allPlayerData.players.forEach(player => {
+      let totalPlayerGold = 0;
+      player.items.forEach(item => {
+        totalPlayerGold = totalPlayerGold + item.price * item.count;
+      });
+      const playerGold: PlayerGold = {
+        championName: player.championName,
+        team: player.team,
+        itemGold: totalPlayerGold,
+      };
+      allPlayerGold.push(playerGold);
+    });
+
+    // for every player
+    // sum all item gold
+    return allPlayerGold;
   }
 }
